74e65fa67d3c9a31b64d9e07e344af023660b045
tboonma <tboonma@hotmail.com>
2022-05-05 23:01:33 +0700

    Implement connection to database

---
+++ connection.py
@@ -0,0 +1,17 @@
+"""
+Connect to database and retrieve cluster data
+"""
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+from decouple import config
+
+def ConnectDatabase():
+    CERT_LOCATION = config('CERT_LOCATION', default='its-no-secret')
+    uri = "mongodb+srv://taobin-project.cfwoi.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
+    client = MongoClient(uri,
+                        tls=True,
+                        tlsCertificateKeyFile=CERT_LOCATION,
+                        server_api=ServerApi('1'))
+
+    db = client['taobin']
+    return db
---
+++ main.py
@@ -0,0 +1,7 @@
+from connection import *
+
+db = ConnectDatabase()
+col = db["customers"]
+
+for x in col.find():
+  print(x)
