2f11851c0e582ed120b73446d7fece385722a5d8
tboonma <tboonma@hotmail.com>
2022-05-05 23:30:30 +0700

    Check

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from connection import *
+# from connection import *
 
-db = ConnectDatabase()
-col = db["customers"]
+# db = ConnectDatabase()
+# col = db["customers"]
 
-for x in col.find():
-  print(x)
+# for x in col.find():
+#   print(x)
+
+CERT_LOCATION = config('CERT_LOCATION', default='its-no-secret')
+print(CERT_LOCATION)
